f5f5c742e7ee11cfd7597bdaa8adda1d5dce081d
bryan_menendez <bryan.menendez@protonmail.com>
2021-04-29 02:02:13 -0300

    initial commit basic serialization

---
+++ getup_restup/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for getup_restup project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'getup_restup.settings')
+
+application = get_wsgi_application()
---
+++ quickstart/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from .serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """api that allows users to be viewed or modified"""
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """api that allows groups to be viewed or modified"""
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
